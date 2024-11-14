aa4225bc42d76a85a96eced8f1e3590234e6dba5
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-11-14 12:25:18 -0500

    feat(exceptions): Add exceptions for the APIs.

--- app/exception_handlers.py
+++ app/exception_handlers.py
@@ -16,7 +16,9 @@ class CreateEntityError(CustomError):
 
 class CreateLocationCategoryReviewedError(CustomError):
     def __init__(
-        self, message: str = "Failed to create location category reviewed.", *args
+        self,
+        message: str = "Failed to create location category reviewed.",
+        *args,
     ):
         super().__init__(message, *args)
 
@@ -24,14 +26,18 @@ class CreateLocationCategoryReviewedError(CustomError):
         return self.args[0]
 
 
-async def handle_custom_error_422(request: Request, exc: CustomError) -> JSONResponse:
+async def handle_custom_error_422(
+    request: Request, exc: CustomError
+) -> JSONResponse:
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content={"message": str(exc)},
     )
 
 
-async def handle_custom_error_404(request: Request, exc: CustomError) -> JSONResponse:
+async def handle_custom_error_404(
+    request: Request, exc: CustomError
+) -> JSONResponse:
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
         content={"message": str(exc)},
@@ -40,4 +46,6 @@ async def handle_custom_error_404(request: Request, exc: CustomError) -> JSONRes
 
 def exception_container(app: FastAPI) -> None:
     app.exception_handler(CreateEntityError)(handle_custom_error_422)
-    app.exception_handler(CreateLocationCategoryReviewedError)(handle_custom_error_404)
+    app.exception_handler(CreateLocationCategoryReviewedError)(
+        handle_custom_error_404
+    )
